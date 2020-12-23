b744f718374a8911149e545fa7a385a6e07771aa
Crew Spence <csspence123@gmail.com>
2020-12-23 06:27:35 -0600

    Wrote working solution.

--- codewars_add_length.js
+++ codewars_add_length.js
@@ -8,6 +8,13 @@ Your task is to write a function that takes a String and returns an Array/list w
 Note: String will have at least one element; words will always be separated by a space.
 */
 
-function addLength(str) {
-//start-here
+const addLength = (str) => {
+  let arr = str.split(' ');
+  let size;
+  for(let i = 0; i < arr.length; i++) {
+    size = arr[i].length;
+    arr[i] += ' ' + size;
+  }
+
+  return arr;
 }
